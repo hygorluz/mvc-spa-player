67a22984af75af38423c60d14cc47b306ad9415c
Hygor Carvalho da Luz <hygor.c.luz@gmail.com>
2024-03-14 20:13:47 -0300

    Creating react app

--- start.sh
+++ start.sh
@@ -13,6 +13,11 @@ echo "Starting the API server..."
 exec gunicorn "controllers.application:create_application()" --workers 1 --worker-class uvicorn.workers.UvicornWorker --threads 8 --max-requests 100 --bind 0.0.0.0:8080 &
 echo $! > gunicorn.pid
 
+echo "Starting the Frontend server..."
+cd views
+SET NODE_OPTIONS=--openssl-legacy-provider
+npm run start
+
 
 
 
